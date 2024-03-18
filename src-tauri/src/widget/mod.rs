27adda6ee319ca2d2f66eb3f284a8e8893bda4d7
Zakaria Mansouri <zakman.dev@gmail.com>
2024-03-18 22:12:02 +0100

    always hide the maximized window

--- src-tauri/src/widget/mod.rs
+++ src-tauri/src/widget/mod.rs
@@ -37,6 +37,7 @@ impl<R: Runtime> WidgetWindow for Window<R> {
                             .maximized(true)
                             .transparent(true)
                             .skip_taskbar(true)
+                            .visible(false)
                             .shadow(false);
 
                         let maximized_window = maximized_window_builder
@@ -50,8 +51,8 @@ impl<R: Runtime> WidgetWindow for Window<R> {
                 };
 
                 let size = PhysicalSize::<i32> {
-                    width: maximized_window.inner_size()?.width as i32,
-                    height: maximized_window.inner_size()?.height as i32,
+                    width: maximized_window.outer_size()?.width as i32,
+                    height: maximized_window.outer_size()?.height as i32,
                 };
 
                 let max_pos: PhysicalPosition<i32> = maximized_window.outer_position()?;
