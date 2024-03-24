aba3a559ff26865e4aefb12c3ec9a418e1fd197b
Zakaria Mansouri <zakman.dev@gmail.com>
2024-03-24 12:00:05 +0100

    using only slint

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,11 @@
-// use winit::{
-//     event::{DeviceEvent, ElementState, Event, KeyboardInput, VirtualKeyCode, WindowEvent},
-//     event_loop::EventLoop,
-//     platform::windows::{WindowExtWindows, HWND},
-//     window::{WindowBuilder, WindowLevel},
-// };
-
-use winit::{
-    dpi::{PhysicalSize, Size},
-    event_loop::EventLoop,
-    window::{WindowBuilder, WindowLevel},
-};
-
+slint::slint! {
+    export component HelloWorld {
+        Text {
+            text: "hello world";
+            color: green;
+        }
+    }
+}
 fn main() {
-    let event_loop = EventLoop::new().unwrap();
-    let window = WindowBuilder::new()
-        .with_transparent(true)
-        .with_visible(true)
-        .with_inner_size(PhysicalSize::new(800, 600))
-        .build(&event_loop)
-        .unwrap();
-    window.set_window_level(WindowLevel::AlwaysOnTop);
-    window.set_cursor_hittest(false).unwrap();
-
-    event_loop.run(move |event, elwt| {
-        //
-    });
+    HelloWorld::new().unwrap().run().unwrap();
 }
