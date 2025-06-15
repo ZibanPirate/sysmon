9d7d4dfdd22cd84aee7a4177129bc16e09638043
Zakaria Mansouri <zakman.dev@gmail.com>
2025-06-15 21:32:27 +0200

    refactor: remove tauri-plugin-opener dependency and clean up related code

--- desktop/src/_entry/mod.rs
+++ desktop/src/_entry/mod.rs
@@ -1,11 +1,9 @@
 use crate::widget::setup::setup_widget;
 
 pub fn app_builder() -> tauri::Builder<tauri::Wry> {
-    tauri::Builder::default()
-        .plugin(tauri_plugin_opener::init())
-        .setup(|app| {
-            setup_widget(app)?;
+    tauri::Builder::default().setup(|app| {
+        setup_widget(app)?;
 
-            Ok(())
-        })
+        Ok(())
+    })
 }
