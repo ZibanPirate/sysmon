094fd705bf07b631e6315baffc4fdee0f16689e8
Zakaria Mansouri <zakman.dev@gmail.com>
2025-07-17 22:07:23 +0200

    Bump Swift tools version to 6.1 in Package.swift

--- lib-swift/swift/Package.swift
+++ lib-swift/swift/Package.swift
@@ -1,5 +1,4 @@
-// swift-tools-version: 5.9
-// 5.9 is the version used in Github Actions
+// swift-tools-version: 6.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
