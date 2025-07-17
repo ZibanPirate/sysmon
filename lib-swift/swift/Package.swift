2bcc5e86fdb8799b2a29223c84dffb182529f2ee
Zakaria Mansouri <zakman.dev@gmail.com>
2025-07-17 19:05:45 +0200

    Set Swift tools version to 5.9 in Package.swift
    5.9 matches the version used in GitHub Actions.

--- lib-swift/swift/Package.swift
+++ lib-swift/swift/Package.swift
@@ -1,4 +1,5 @@
-// swift-tools-version: 6.1
+// swift-tools-version: 5.9
+// 5.9 is the version used in Github Actions
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
