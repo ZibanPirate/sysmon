caa5db2b4bb77fb74b9062839dc0c5dd30adeea6
Zakaria Mansouri <zakman.dev@gmail.com>
2025-07-23 16:35:44 +0200

    Use app_config_dir instead of config_dir for settings path

--- desktop/src/settings/persist.rs
+++ desktop/src/settings/persist.rs
@@ -3,9 +3,8 @@ use common_types::settings::Settings;
 use std::fs::create_dir_all;
 use tauri::{AppHandle, Manager};
 
-// todo-zm: make sure settings_path is the right place to store settings
 pub fn persist_settings(settings: &Settings, app_handle: AppHandle) -> Result<()> {
-    let config_dir = app_handle.path().config_dir()?;
+    let config_dir = app_handle.path().app_config_dir()?;
     create_dir_all(&config_dir)?;
     let settings_path = config_dir.join("settings.json");
     let file = std::fs::File::create(&settings_path)?;
@@ -15,7 +14,7 @@ pub fn persist_settings(settings: &Settings, app_handle: AppHandle) -> Result<()
 }
 
 pub fn load_settings(app_handle: &AppHandle) -> Result<Settings> {
-    let config_dir = app_handle.path().config_dir()?;
+    let config_dir = app_handle.path().app_config_dir()?;
     let settings_path = config_dir.join("settings.json");
     if !settings_path.exists() {
         return Ok(Settings::default());
